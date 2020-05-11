db797bc7f928dac5f4ed001e385a9deccc164337
Noah Walsmits <noahwalsmits@telfort.nl>
2020-05-11 15:50:57 +0200

    Implemented GameObject skeleton
    very spooky

--- Proftaak_AR/Proftaak_AR/src/gamelogic/GameObject.cpp
+++ Proftaak_AR/Proftaak_AR/src/gamelogic/GameObject.cpp
@@ -1,1 +1,14 @@
 #include "GameObject.h"
+
+GameObject::GameObject(CollisionObject2D collisionObject)
+{
+	this->collisionObject = collisionObject;
+}
+
+void GameObject::update()
+{
+}
+
+void GameObject::draw()
+{
+}
--- Proftaak_AR/Proftaak_AR/src/gamelogic/GameObject.h
+++ Proftaak_AR/Proftaak_AR/src/gamelogic/GameObject.h
@@ -1,5 +1,22 @@
 #pragma once
+#include <gamelogic\CollisionObject2D.h>
+
 class GameObject
 {
+private:
+	//TODO Mesh
+	//TODO Shader
+	//TODO Texture
+	CollisionObject2D collisionObject;
+
+public:
+	//TODO add mesh, shader and texture in the future
+	GameObject(CollisionObject2D collisionObject);
+
+	/* Updates this gameobject */
+	void update();
+
+	/* Draws this gameobject */
+	void draw();
 };
 
