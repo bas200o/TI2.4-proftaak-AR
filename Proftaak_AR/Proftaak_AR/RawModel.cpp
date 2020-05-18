5d4bcc9dafbad943b8c75f673a84dbf2ea2f2716
Luke Taylor <lip.taylor@student.avans.nl>
2020-05-18 10:02:56 +0200

    This time without filter :)

---
+++ Proftaak_AR/Proftaak_AR/RawModel.cpp
@@ -0,0 +1,6 @@
+#include"RawModel.h"
+
+OpenGL::RawModel::RawModel(int voaID, int vertexCount)
+	:vaoID(vaoID), vertexCount(vertexCount) {
+
+}
--- Proftaak_AR/Proftaak_AR/RawModel.h
+++ Proftaak_AR/Proftaak_AR/RawModel.h
@@ -1,1 +1,18 @@
 #pragma once
+
+
+namespace OpenGL
+{
+	class RawModel
+	{
+		
+	private: 
+		int vaoID;
+		int vertexCount;
+	public:
+		RawModel(int vaoID, int vertexCount);
+		int getVaoID() { return this->vaoID; };
+		int getVaoID() { return this->vertexCount; };
+
+	};
+}
