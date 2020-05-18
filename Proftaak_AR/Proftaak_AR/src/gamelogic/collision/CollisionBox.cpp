24e7b49992d579d2f0ad6bb2f09db6a68eae7be4
Noah Walsmits <noahwalsmits@telfort.nl>
2020-05-18 12:37:19 +0200

    Updated collision check method

--- Proftaak_AR/Proftaak_AR/src/gamelogic/collision/CollisionBox.cpp
+++ Proftaak_AR/Proftaak_AR/src/gamelogic/collision/CollisionBox.cpp
@@ -1,5 +1,7 @@
 #include "CollisionBox.h"
 
+
+
 CollisionBox::CollisionBox(glm::vec2 position, double angle, double width, double height)
 {
 	this->position = position;
@@ -10,24 +12,68 @@ CollisionBox::CollisionBox(glm::vec2 position, double angle, double width, doubl
 
 bool CollisionBox::collidesWith(CollisionBox collisionBox)
 {
-	//TODO
-	//figure out the location of the corners
-	glm::vec2 TL1 = this->position; //top left
-	glm::vec2 TR1 = this->position + glm::vec2(cos(angle), sin(angle)); //top right
-	glm::vec2 BL1 = this->position + glm::vec2(cos(angle + 270.0), sin(angle + 270.0)); //bottom left
-	glm::vec2 BR1 = BL1 + glm::vec2(cos(angle), sin(angle)); //bottom right
+	//TODO test
+	//TODO fix warnings
+	//TODO move other box accroding to collision direction?
+	std::vector<glm::vec2> rectangles[] = { this->getPoints(), collisionBox.getPoints() };
 
-	glm::vec2 TL2 = collisionBox.getPosition(); //top left
-	glm::vec2 TR2 = collisionBox.getPosition() + glm::vec2(cos(angle), sin(angle)); //top right
-	glm::vec2 BL2 = collisionBox.getPosition() + glm::vec2(cos(angle + 270.0), sin(angle + 270.0)); //bottom left
-	glm::vec2 BR2 = BL2 + glm::vec2(cos(angle), sin(angle)); //bottom right
-
-	for (int i = 0; i < 2; i++)
+	for (std::vector<glm::vec2> points : rectangles) //for each box
 	{
-		
+		for (int i1 = 0; i1 < points.size(); i1++) //for each corner
+		{
+			int   i2 = (i1 + 1) % points.size();
+			glm::vec2 p1 = points[i1];
+			glm::vec2 p2 = points[i2];
+
+			glm::vec2 normal = glm::vec2(p2.y - p1.y, p1.x - p2.x);
+
+			double minA = std::numeric_limits<double>::max();
+			double maxA = std::numeric_limits<double>::min();
+			for (glm::vec2 point : this->getPoints()) //this box
+			{
+				double projected = normal.x * point.x + normal.y * point.y;
+
+				if (projected < minA) {
+					minA = projected;
+				} if (projected > maxA) {
+					maxA = projected;
+				}
+			}
+
+			double minB = std::numeric_limits<double>::max();
+			double maxB = std::numeric_limits<double>::min();
+			for (glm::vec2 point : collisionBox.getPoints()) //other box
+			{
+				double projected = normal.x * point.x + normal.y * point.y;
+
+				if (projected < minB) {
+					minB = projected;
+				} if (projected > maxB) {
+					maxB = projected;
+				}
+			}
+			if (maxA < minB || maxB < minA) {
+				return false;
+			}
+		}
+
 	}
 
 	return false;
+}
+
+std::vector<glm::vec2> CollisionBox::getPoints()
+{
+	glm::vec2 TL = this->position; //top left
+	glm::vec2 TR = this->position + glm::vec2(cos(angle), sin(angle)); //top right
+	glm::vec2 BL = this->position + glm::vec2(cos(angle + 270.0), sin(angle + 270.0)); //bottom left
+	glm::vec2 BR = BL + glm::vec2(cos(angle), sin(angle)); //bottom right
+	std::vector<glm::vec2> points = std::vector<glm::vec2>();
+	points.push_back(TL);
+	points.push_back(TR);
+	points.push_back(BL);
+	points.push_back(BL);
+	return points;
 }
 
 glm::vec2 const& CollisionBox::getPosition()
--- Proftaak_AR/Proftaak_AR/src/gamelogic/collision/CollisionBox.h
+++ Proftaak_AR/Proftaak_AR/src/gamelogic/collision/CollisionBox.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <glm/vec2.hpp>
+#include <vector>
 
 class CollisionBox
 {
@@ -10,11 +11,16 @@ private:
 	double width;
 	double height;
 
+protected:
+	std::vector<glm::vec2> getPoints();
+
 public:
 	CollisionBox(glm::vec2 position, double angle, double width, double height);
 
+	/* Checks if this box collides with another */
 	bool collidesWith(CollisionBox collisionBox);
 
+	/* Returns  the positions of the corners of this box */
 	glm::vec2 const& getPosition();
 
 	double const& getAngle();
