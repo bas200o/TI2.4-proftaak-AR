a6af1104d00787509ba62ec5301c9bfe9ecdb94a
Luke Taylor <lip.taylor@student.avans.nl>
2020-05-18 11:55:41 +0200

    Loader finished

--- Proftaak_AR/Proftaak_AR/src/OpenGL/Renderer/Loader.h
+++ Proftaak_AR/Proftaak_AR/src/OpenGL/Renderer/Loader.h
@@ -1,16 +1,21 @@
 #pragma once
 
 #include "RawModel.h"
-#include <GLFW/glfw3.h>
 #include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <vector>
+
 
 namespace OpenGL {
 	class Loader {
 	private:
 		int createVAO();
-		void storeDataInAttributeList(int attributeNumber, float data[]);
+		void storeDataInAttributeList(int attributeNumber, std::vector<float> data);
 		void unbindVao();
+		std::vector<unsigned int> vaos;
+		std::vector<unsigned int> vbos;
 	public:
-		RawModel loadToVAO(float positions[]);
+		RawModel loadToVAO(std::vector<float> postitions);
+		void cleanup();
 	};
 }
