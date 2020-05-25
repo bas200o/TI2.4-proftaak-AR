d956723076b1dd259e1ccff2e1d5e633a96ecde8
Noah Walsmits <noahwalsmits@telfort.nl>
2020-05-25 16:55:07 +0200

    Added collision testing method

--- Proftaak_AR/Proftaak_AR/src/Main.cpp
+++ Proftaak_AR/Proftaak_AR/src/Main.cpp
@@ -16,12 +16,30 @@ int main()
 void collisionTest() 
 {
 	CollisionBox2D box1 = CollisionBox2D(glm::vec2(0.0, 0.0), 90.0, 10.0, 5.0);
-	std::array<glm::vec2, 4> corners = box1.getCorners();
-	box1.setCenter(glm::vec2(10.0, 10.0));
+	CollisionBox2D box2 = CollisionBox2D(glm::vec2(0.0, 0.0), 90.0, 10.0, 5.0);
 
-	CollisionBox2D box2 = CollisionBox2D(glm::vec2(5.1, 0.0), 90.0, 10.0, 5.0);
+	//test 1
+	box1.setCenter(glm::vec2(4.9, 0.0));
+	std::cout << "test 1 is: " << box1.collidesWith(box2) << " should be: " << 1 << "\n"; //should be 1
 
-	std::cout << box1.collidesWith(box2);
+	//test 2
+	box1.setCenter(glm::vec2(5.1, 0.0));
+	std::cout << "test 2 is: " << box1.collidesWith(box2) << " should be: " << 0 << "\n"; //should be 0
+
+	//test 3
+	box1.setCenter(glm::vec2(0.0, 0.0));
+	box2.rotate(-90.0);
+	std::cout << "test 3 is: " << box1.collidesWith(box2) << " should be: " << 1 << "\n"; //should be 1
+
+	//test 4
+	box1.setCenter(glm::vec2(0.0, 0.0));
+	box2.push(7.4, 0.0);
+	std::cout << "test 4 is: " << box1.collidesWith(box2) << " should be: " << 1 << "\n"; //should be 1
+
+	//test 5
+	box1.setCenter(glm::vec2(0.0, 0.0));
+	box2.push(0.2, 0.0);
+	std::cout << "test 5 is: " << box1.collidesWith(box2) << " should be: " << 0 << "\n"; //should be 0
 }
 
 /*TODO
